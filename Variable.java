21722ecb29064fa8466e711f0c85510669ebcecc
Vignesh Moorthy <madhanvic@gmail.com>
2024-03-01 06:44:20 +0530

    Hello world and variables

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name = "Vignesh";
+        String dob = new String("19-06-2000");
+        System.out.println("I'am " + name + " and my date of birth is " + dob);
+        name = "Ramya";
+        dob = "29/03/2002";
+        System.out.println("I'am " + name + " and my date of birth is " + dob);
+        System.out.println("Difference the age between Ramya and Vignesh : " + (2002 - 2000));
+    }
+}
